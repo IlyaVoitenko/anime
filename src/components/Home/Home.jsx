dcee635b2523a9e0c923709bbcb5455c429cb637
Voitenko Illia <79865476+IlyaVoitenko@users.noreply.github.com>
2023-10-28 17:13:03 +0200

    added sliders

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,32 +1,46 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import styles from "./Home.module.css";
 
 const Home = () => {
-  // const [image, setImage] = useState("");
-  const [content, setContent] = useState(null);
+  const [data, setData] = useState(null);
   useEffect(() => {
     const getTopAnime = async () => {
       const { data } = await axios.get(`https://api.jikan.moe/v4/top/anime`);
-      console.log(data.data[0]);
-      // setImage(data.data[0].images.jpg.image_url);
-      setContent({
-        name: data.data[0].title_english,
-        title: data.data[0].synopsis,
-      });
+      console.log(data.data);
+      console.log(data?.data[0].images.jpg.large_image_url);
+      setData(data);
       return data;
     };
     getTopAnime();
   }, []);
   return (
-    <section className="home" id="home">
+    <section className="p-0" id="home">
       <div className="home-slider">
         <div className="wrappen">
           <div className="slide">
-            <div className={styles.box}>
+            <div
+              className={`flex text-center justify-end pt-[2rem] pr-[9%] bg-cover	bg-center	 bg-[url('https://cdn.myanimelist.net/images/anime/1208/94745l.jpg')] bg-no-repeat min-h-screen`}
+            >
               <div className="content">
-                <h3>{content?.name}</h3>
-                <p>{content?.title}</p>
+                <h3>{data?.data[0].title_english}</h3>
+                <p>{data?.data[0].synopsis}</p>
+                <a href="#" className="btn">
+                  download
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="slide">
+            <div
+              className={`flex text-center justify-start pt-[2rem] pr-[9%] bg-cover	bg-center	bg-[url('https://cdn.myanimelist.net/images/anime/1935/127974l.jpg')] bg-no-repeat min-h-screen`}
+            >
+              <div className="content">
+                <h3>{data?.data[1].title_english}</h3>
+                <p>{data?.data[1].synopsis}</p>
+                <a href="#" className="btn">
+                  download
+                </a>
               </div>
             </div>
           </div>
